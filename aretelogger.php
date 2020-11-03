cf8548e9f9fae159e852341376fb09f6f8b8f265
Skooz <gadzooks297@gmail.com>
2020-11-03 08:40:53 -0500

    logger updated, now appends to files, etc.

--- aretelogger.php
+++ aretelogger.php
@@ -7,7 +7,7 @@ require_once('rabbitMQLib.inc');
 // Write\append to log file
 function makeLog($log)
 {
-	$logfile = fopen("log.txt", "w") or die ("Error opening log file");	
+	$logfile = fopen("log.txt", "a") or die ("Error opening log file");	
 	$message = date("Y-m-d | h:i:sa")." | ".$log.PHP_EOL;
 	echo $message;
 	return fwrite($logfile, $message);
@@ -25,7 +25,7 @@ function requestProcessor($request)
   switch ($request['type'])
   {
     case "log":
-	  return makeLog($request['log']);
+	  return makeLog($request['msg']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
